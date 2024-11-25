3111d4d894a8ac257eb9df795ecdef40a634dea5
jinxudong <18751241086@163.com>
2024-11-25 21:20:19 +0800

    移动端样式

--- edit-ai/src/app/layout.tsx
+++ edit-ai/src/app/layout.tsx
@@ -1,35 +1,44 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './custom.css'
 import './globals.css'
-
 import { ThemeProvider } from '@/components/theme-provider'
+import { isMobileDevice } from '@/lib/isMobileDevice'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: '划水 AI',
   description: 'AIGC 知识库，AI 写作，协同编辑',
-};
+}
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
+  const isMobile = await isMobileDevice() // 这个函数是异步的？？？
+
   return (
     <html lang="en" suppressHydrationWarning>
-      {/* <body className={inter.className}>{children}</body> */}
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        {isMobile && (
+          <div className="h-screen flex flex-col items-center justify-center">
+            <p>
+              <strong>划水 AI</strong> ，暂不支持移动端浏览器~
+            </p>
+          </div>
+        )}
+        {!isMobile && (
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        )}
       </body>
     </html>
-  );
+  )
 }
